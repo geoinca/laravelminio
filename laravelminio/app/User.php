0582fbe560526029b41a5a7db66f015a6f204cde
@geo_inca <geo.inca@hotmail.com>
2020-04-26 15:56:41 +0200

    [Ticket:  0002 ]
    add roles to user
    
    add Role model
    modified User model
    
    create migration  create_roles_table
    create migration  create_role_user_table
    
    add seeds  Role and User TableSeeder
    
    	modified:   app/Http/Controllers/Auth/RegisterController.php
    	modified:   app/Http/Controllers/HomeController.php
    	new file:   app/Role.php
    	modified:   app/User.php
    	new file:   database/migrations/2020_04_26_084026_create_roles_table.php
    	new file:   database/migrations/2020_04_26_084544_create_role_user_table.php
    	modified:   database/seeds/DatabaseSeeder.php
    	new file:   database/seeds/RoleTableSeeder.php
    	new file:   database/seeds/UserTableSeeder.php

--- laravelminio/app/User.php
+++ laravelminio/app/User.php
@@ -32,4 +32,40 @@ class User extends Authenticatable
     {
         return $this->hasMany(Post::class);
     }
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
+    /*** 
+     * function  authorizeRoles,hasAnyRole, hasAnyRole for user 
+     */
+    public function authorizeRoles($roles)
+    {
+        if ($this->hasAnyRole($roles)) {
+            return true;
+        }
+        abort(401, 'Esta acción no está autorizada.');
+    }
+    public function hasAnyRole($roles)
+    {
+        if (is_array($roles)) {
+            foreach ($roles as $role) {
+                if ($this->hasRole($role)) {
+                    return true;
+                }
+            }
+        } else {
+            if ($this->hasRole($roles)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public function hasRole($role)
+    {
+        if ($this->roles()->where('name', $role)->first()) {
+            return true;
+        }
+        return false;
+    }
 }
