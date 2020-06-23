14ed7d0d54d1435f4337008496270e452cd6e88a
@geo_inca <geo.inca@hotmail.com>
2020-06-23 17:33:52 +0200

    [Ticket: 0001.c] Convert docker-compose
    	modified:   docker-compose.yml

--- laravelminio/routes/web.php
+++ laravelminio/routes/web.php
@@ -16,10 +16,10 @@ Route::group(['middleware' => 'auth'], function () {
 //Route::get('/', 'UsersController@index');
 Route::get('/', function () {
 
-    $created = Storage::disk('minio')->put('/test.txt','Hello World!');
-    print_r($created);
-    $files = Storage::disk('minio')->files('/');
-    print_r($files);
+    // $created = Storage::disk('minio')->put('/test.txt','Hello World!');
+    // print_r($created);
+    // $files = Storage::disk('minio')->files('/');
+    // print_r($files);
     $s3 = new Aws\S3\S3Client([
         'version' => 'latest',
         'region'  => env('MINIO_REGION'),
@@ -32,50 +32,48 @@ Route::get('/', function () {
     ]);
     //
     //$created2=$s3::disk('minio')->put('/tests3.txt','Hello World!');
-$buckets=$s3->listBuckets();
-foreach ($buckets['Buckets'] as $bucket) {
-    echo $bucket['Name'] . "<br>\n";
-}
-//Gets the access control policy for a bucket
-//json_encode($buckets);
-//dd($buckets);
-$bucket = 'media';
-
-$result=$s3->listObjects(array('Bucket'=>$bucket));
- 
-echo "keys retrived !\n";
-foreach ($result['Contents'] as $object){
-    echo $object["Key"] . "<br>\n";
-}
-// try {
-//     $resp = $s3->getBucketAcl([
-//         'Bucket' => $bucket
-//     ]);
-//     echo "Succeed in retrieving bucket ACL as follows: \n";
-//     var_dump($resp);
-// } catch (AwsException $e) {
-//     // output error message if fails
-//     echo $e->getMessage();
-//     echo "\n";
-// }
-// dd($buckets);
+        $buckets=$s3->listBuckets();
+        foreach ($buckets['Buckets'] as $bucket) {
+            echo $bucket['Name'] . "<br>\n";
+        }
+    //Gets the access control policy for a bucket
+    //json_encode($buckets);
+    dd($buckets);
+    $bucket = 'media';
+    $result=$s3->listObjects(array('Bucket'=>$bucket));
+    echo "keys retrived !\n";
+    foreach ($result['Contents'] as $object){
+        echo $object["Key"] . "<br>\n";
+    }
+    // try {
+    //     $resp = $s3->getBucketAcl([
+    //         'Bucket' => $bucket
+    //     ]);
+    //     echo "Succeed in retrieving bucket ACL as follows: \n";
+    //     var_dump($resp);
+    // } catch (AwsException $e) {
+    //     // output error message if fails
+    //     echo $e->getMessage();
+    //     echo "\n";
+    // }
+    // dd($buckets);
 
 
 
 
-// // Send a PutObject request and get the result object.
-// $insert = $s3->putObject([
-//     'Bucket' => 'media',
-//     'Key'    => 'testkey',
-//     'Body'   => 'Hello from MinIO!! we∫we∫we∫we∫we∫we∫we∫we∫'
-// ]);
-// dd($insert );
-// // Download the contents of the object.
-// $retrive = $s3->getObject([
-//     'Bucket' => 'media',
-//     'Key'    => 'testkey',
-//     'SaveAs' => 'testkey_local'
-// ]);
+    // // Send a PutObject request and get the result object.
+    // $insert = $s3->putObject([
+    //     'Bucket' => 'media',
+    //     'Key'    => 'testkey',
+    //     'Body'   => 'Hello from MinIO!! we∫we∫we∫we∫we∫we∫we∫we∫'
+    // ]);
+    // dd($insert );
+    // // Download the contents of the object.
+    // $retrive = $s3->getObject([
+    //     'Bucket' => 'media',
+    //     'Key'    => 'testkey',
+    //     'SaveAs' => 'testkey_local'
+    // ]);
 
 });
 
